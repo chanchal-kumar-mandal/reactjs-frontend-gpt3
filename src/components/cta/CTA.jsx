6b2cc444c74ff9aeaef70fd397942188e15482fe
chanchal-kumar-mandal <ckmandal9@gmail.com>
2022-04-17 00:05:33 +0530

    GPT-3 frontend on ReactJS

---
+++ src/components/cta/CTA.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './cta.css'
+
+const CTA = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CTA
---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './whatGPT3.css'
+
+const WhatGPT3 = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default WhatGPT3
